90fe4dc883e87f35246fb06534c6aa89892b7c02
Kim Minseok <semteul32@gmail.com>
2025-04-30 12:47:42 +0900

    feature: 채팅 자동 스크롤
    - 새 채팅이 올라왔을 때 자동으로 스크롤하는 기능 추가

--- src/components/chat/message/MessageList.tsx
+++ src/components/chat/message/MessageList.tsx
@@ -1,6 +1,6 @@
 "use client"
 import useChatSync from "@/hooks/useChatSync";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {ChatMessage} from "@/types/chat";
 import {User} from "@/types/user";
 import SentMessage from "@/components/chat/message/SentMessage";
@@ -8,7 +8,7 @@ import ReceivedMessage from "@/components/chat/message/ReceivedMessage";
 import BotMessage from "@/components/chat/message/BotMessage";
 import useUserInfo from "@/hooks/useUserInfo";
 
-function MessageBuilder({message}: { message: ChatMessage }) {
+function MessageBuilder({message}: { message: ChatMessage}) {
   const [me, setMe] = useState<User | null>(null);
   const [sender, setSender] = useState<User | null>(null);
 
@@ -47,7 +47,13 @@ function MessageBuilder({message}: { message: ChatMessage }) {
 }
 
 export default function MessageList({uuid}: { uuid: string }) {
+    const ref =  useRef<HTMLDivElement>(null);
   const [messages, setMessages] = useState<Array<ChatMessage>>([]);
+
+  useEffect(()=>{
+      if (ref.current)
+          ref.current.scrollIntoView();
+  },[messages]);
 
   const update = (messages: Array<ChatMessage>) => {
     setMessages(messages);
@@ -57,8 +63,11 @@ export default function MessageList({uuid}: { uuid: string }) {
 
   return <div>
     {
-      messages.map((message) => {
-        return <MessageBuilder key={message.chatId} message={message}/>;
+      messages.map((message,index) => {
+        if (index === messages.length-1) {
+            return <div key={message.chatId} ref={ref}><MessageBuilder message={message}/></div>;
+        }
+        return <div key={message.chatId}><MessageBuilder message={message}/></div>;
       })
     }
   </div>
