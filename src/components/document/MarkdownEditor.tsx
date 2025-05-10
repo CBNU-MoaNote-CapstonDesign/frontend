ccf903a0980de15cea06ad67675bcaff2e5919ef
SONY-STRING <sctoa1@gmail.com>
2025-05-11 03:57:42 +0900

    fix(component/document): `documentRenderer` 컴포넌트에서 `MarkdownRenderer` 를 클릭 시에 수정 사항을 전파하는 현상 해결 #45
    - 기존에는 `MarkdownEditor` 의 mount 시 `useEffect` 가 실행되며 조건없이 `updateContent` 를 호출하였음
    - 또한 `updateContent` 는 `documentRenderer` 의 `send` 를 호출함 (`documentRenderer` 에서 그렇게 정의함) 따라서 실제로 수정이 일어나지 않았음에도 `MarkdownEditor` 가 호출될 시에 수정 사항을 외부로 전파함
    - 따라서 문서 수정에 대한 `useEffect` 내부에서 `needSend` 조건을 체크하도록 변경

--- src/components/document/MarkdownEditor.tsx
+++ src/components/document/MarkdownEditor.tsx
@@ -6,7 +6,7 @@ import TextareaAutosize from "react-textarea-autosize";
 /**
  * 마크다운 에디터
  * @param initialContent 초기 내용
- * @param updateContent 사용자가 내용 업데이트할 때 콜백, 상위 컴포넌트에서 서버로 업데이트 해야 함
+ * @param updateContent 에디터에서 내용 업데이트할 때 콜백
  * @param updateBlur 사용자가 에디터의 포커스를 잃었을 때 콜백
  * @param lastCursorPosition 커서 위치 초기값
  * @param cursorHandler 커서 위치를 상위 컴포넌트로 전달하는 콜백
@@ -20,7 +20,6 @@ export function MarkdownEditor({initialContent, updateContent = null, updateBlur
   cursorHandler: ((cursor: number) => void)
 }) {
 
-  const [content, setContent] = useState(initialContent ? initialContent : "");
   const [cursorPosition, setCursor] = useState(lastCursorPosition);
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
@@ -34,12 +33,6 @@ export function MarkdownEditor({initialContent, updateContent = null, updateBlur
   }
 
   useEffect(() => {
-    if (updateContent) {
-      updateContent(content);
-    }
-  }, [content]);
-
-  useEffect(() => {
     if (textAreaRef.current) {
       textAreaRef.current.focus();
       textAreaRef.current.setSelectionRange(cursorPosition, cursorPosition);
@@ -49,8 +42,8 @@ export function MarkdownEditor({initialContent, updateContent = null, updateBlur
   // TODO : change 발생 시 api 로 백엔드 호출해 적용시키기, 상위 컴포넌트에서 할 것
   const handleChange = () => {
     const el = textAreaRef.current;
-    if (el) {
-      setContent(el.value);
+    if (el && updateContent) {
+      updateContent(el.value);
     }
   }
 
@@ -65,7 +58,7 @@ export function MarkdownEditor({initialContent, updateContent = null, updateBlur
         className="w-full border p-2 rounded-xl"
         minRows={5}
         placeholder={"Type Document Here..."}
-        value={content ? content : ""}
+        value={initialContent ? initialContent : ""}
         onBlur={handleBlur}
         onChange={handleChange}
         onSelect={handleSelect}
