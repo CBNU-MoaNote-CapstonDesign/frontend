c97e6d02cae2df482a882593c720defcc1f3fbbf
SONY-STRING <sctoa1@gmail.com>
2025-05-11 14:46:15 +0900

    refactor(MarkdownEditor): `lastCursorPosition` 과 `cursorHandler` prop 을 optional 로 변경
    - initialContent 의 기본값을 null 로 수정

--- src/components/document/MarkdownEditor.tsx
+++ src/components/document/MarkdownEditor.tsx
@@ -6,30 +6,31 @@ import TextareaAutosize from "react-textarea-autosize";
 /**
  * 마크다운 에디터
  * @param initialContent 초기 내용
- * @param updateContent 에디터에서 내용 업데이트할 때 콜백
- * @param updateBlur 사용자가 에디터의 포커스를 잃었을 때 콜백
+ * @param updateContent 에디터에서 내용 업데이트할 때 콜백 (optional)
+ * @param updateBlur 사용자가 에디터의 포커스를 잃었을 때 콜백 (optional)
  * @param lastCursorPosition 커서 위치 초기값
- * @param cursorHandler 커서 위치를 상위 컴포넌트로 전달하는 콜백
+ * @param cursorHandler 커서 위치를 상위 컴포넌트로 전달하는 콜백 (optional)
  * @constructor
  */
-export function MarkdownEditor({initialContent, updateContent = null, updateBlur = null, lastCursorPosition, cursorHandler}: {
+export function MarkdownEditor({initialContent = null, updateContent = null, updateBlur = null, lastCursorPosition = null, cursorHandler = null}: {
   initialContent: string | null | undefined,
   updateContent?: null | ((content: string) => void),
   updateBlur?: null | (() => void),
-  lastCursorPosition: number,
-  cursorHandler: ((cursor: number) => void)
+  lastCursorPosition?: number | null | undefined,
+  cursorHandler?: null | ((cursor: number) => void)
 }) {
 
-  const [cursorPosition, setCursor] = useState(lastCursorPosition);
+  const [cursorPosition, setCursor] = useState(lastCursorPosition ? lastCursorPosition : 0);
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
   const handleSelect = () => {
     const el = textAreaRef.current;
     invariant(el, "textAreaRef is null");
-    if (el) {
-      setCursor(el.selectionStart);
+    if (!el)
+      return;
+    setCursor(el.selectionStart);
+    if (cursorHandler)
       cursorHandler(el.selectionStart);
-    }
   }
 
   useEffect(() => {
