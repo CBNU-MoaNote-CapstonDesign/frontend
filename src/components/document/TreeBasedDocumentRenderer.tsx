738b94ed33f19daf08a3fb991ac0380a126cab7d
Kim Minseok <semteul32@gmail.com>
2025-05-27 15:48:08 +0900

    feature(fugue): fugue 동작을 주고 받는 STOMP API 를 위한 sync hook 구현 및 기존 컴포너트가 hook 을 사용하도록 함
    Co-authored-by: SONY-STRING <sctoa1@gmail.com>

--- src/components/document/TreeBasedDocumentRenderer.tsx
+++ src/components/document/TreeBasedDocumentRenderer.tsx
@@ -7,46 +7,54 @@ import {MarkdownRenderer} from "@/components/document/MarkdownRenderer";
 import DocumentTitle from "@/components/document/DocumentTitle";
 import {TreeNote} from "@/libs/structures/treenote";
 import getDiff from "@/libs/simpledDiff";
+import {CRDTOperation} from "@/types/crdtOperation";
+import useFugueDocumentSync from "@/hooks/useFugueDocumentSync";
 
-export default function DocumentRenderer() {
+export default function DocumentRenderer({user, uuid}: { user:User, uuid: string }) {
   // TODO 임시 데이터
-  const [treeNote, setTreeNote] = useState<TreeNote>(TreeNote.fromString("userid", "noteid", "initial title", "initial content"));
+  const [treeNote, setTreeNote] = useState<TreeNote>(TreeNote.fromString(user.id, uuid, "initial title", "initial content"));
 
-  const [document, setDocument] = useState<Note>({title: treeNote.title, id: treeNote.id, content: treeNote.content}); // 현재 문서 내용 // 현재 문서 내용
+  const [document, setDocument] = useState<Note>({title: treeNote.title, id: treeNote.id, content: treeNote.content}); // 현재 문서 내용
   const [isEditing, setEditing] = useState<boolean>(false); // 현재 편집중인가?
   const [cursorPosition, setCursorPosition] = useState<number>(0); // 커서 위치
-  // const [needSend, setNeedSend] = useState<boolean>(false); // 수정 사항을 보내야 하는지
-
   const startEditing = () => setEditing(true);
   const endEditing = () => setEditing(false);
 
-  // // 다른데서 전파한 사항을 업데이트 하는거
-  // const update =
-  //   (content: string) => {
-  //     console.log(content);
-  //     return;
-  //   };
+  const commitActions = () => {
+    treeNote.traversal();
+    console.log('applied : ' + treeNote.content);
+    setDocument({
+      ...document,
+      content: treeNote.content,
+    });
+    setTreeNote(treeNote);
+  }
 
-  // const {publish} = useDocumentSync(user, uuid, update);
+  // 다른데서 전파한 사항을 업데이트 하는거
+  const update =
+    (actions: CRDTOperation[]) => {
+      console.log("Received actions: ", actions);
+      for (const action of actions) {
+        if (action.byWho === user.id) {
+          console.log("Ignoring action by self");
+          return; // 자신이 보낸건 무시
+        }
+        treeNote.onAction(action);
+      }
+      commitActions();
+    };
 
-  // // 로컬 변경사항을 보내는거
-  // const send =
-  //   (action: Action) => {
-  //     console.log("Send", action);
-  //     return;
-  //   };
 
-  // useEffect(() => {
-  //   if (needSend) {
-  //     setNeedSend(false);
-  //     send(document.content);
-  //   }
-  //   /*
-  //    * needSend 는 dependency 에 포함하지 말아야 함.
-  //    * useEffect 수정 시 dependency 목록을 변경해야 하는 지 확인하는 것이 좋음 (경고가 꺼져있으므로 직접 확인해야 함)
-  //    */
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [document.content, document.id, document.title, send]);
+  const [{broadcast}, ] = useState(useFugueDocumentSync(uuid, update));
+
+
+
+  // 로컬 변경사항을 보내는거
+  const send =
+    (actions: CRDTOperation[]) => {
+      console.log("Send ", actions);
+      broadcast(actions);
+    };
 
   return (
     <div className={"flex flex-col w-full"} key={treeNote.id}>
@@ -60,16 +68,11 @@ export default function DocumentRenderer() {
 
             if (diff.insertedContent)
               treeNote.insert(diff.removeFrom, diff.insertedContent);
-            if (diff.removeLength > 0) {
+            if (diff.removeLength > 0)
               treeNote.remove(diff.removeFrom, diff.removeLength);
-            }
-            treeNote.traversal();
-            console.log('applied : ' + treeNote.content);
-            setDocument({
-              ...document,
-              content: treeNote.content,
-            });
-            setTreeNote(treeNote);
+
+            send(treeNote.operationHistories[treeNote.operationHistories.length - 1]);
+            commitActions();
           }}
           lastCursorPosition={cursorPosition}
           cursorHandler={setCursorPosition}
