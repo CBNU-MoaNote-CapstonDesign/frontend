db6ec82d301599ad8b11203919c6730246aa0ba6
Sa Jaeheon <jls15900@gmail.com>
2025-05-27 19:53:59 +0900

    design: 더 알아보기 버튼에 커서 포인터 효과를 추가

--- src/components/layout/Home/Final.tsx
+++ src/components/layout/Home/Final.tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/navigation';
 function SeeMoreButton({ text, onClick }: { text: string, onClick: () => void }) {
     return (
         <button
-            className="mt-6 bg-[#186370] hover:bg-[#1e7a8a] text-white px-10 py-3 text-2xl font-semibold rounded-full shadow-md transition"
+            className="mt-6 bg-[#186370] hover:bg-[#1e7a8a] text-white px-10 py-3 text-2xl font-semibold rounded-full shadow-md transition cursor-pointer"
             onClick={onClick}>
             {text}
         </button>
