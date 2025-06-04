ee7b745f98c1d67acd28e9fc5f8991a1fd43f056
Sa Jaeheon <jls15900@gmail.com>
2025-06-04 17:11:17 +0900

    BugFix: 노트 링크로 이동시 화면이 살짝 밑으로 내려가던 버그 수정

--- src/app/doc/layout.tsx
+++ src/app/doc/layout.tsx
@@ -4,7 +4,7 @@ export default function DocumentLayout({
   children: React.ReactNode;
 }>) {
   return <div className={"flex flex-col h-screen"}>
-    <div className={"flex-1 w-full m-0 p-5"}>
+    <div className={"flex-1 w-full m-0 p-0"}>
       {children}
     </div>
   </div>
