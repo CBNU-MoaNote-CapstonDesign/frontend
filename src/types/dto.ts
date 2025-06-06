13b1d3e66e55968fa51754e994f8ad2678419bd2
Kim Minseok <semteul32@gmail.com>
2025-06-06 19:22:39 +0900

    feature(file): 파일 구조 데이터 추가

---
+++ src/types/dto.ts
@@ -0,0 +1,50 @@
+import {UUID} from "node:crypto";
+
+export interface FileDTO {
+  id: UUID,
+  name: string,
+  type: FileTypeDTO,
+  dir: UUID,
+}
+
+export enum FileTypeDTO {
+  DOCUMENT,
+  DIRECTORY,
+}
+
+export interface NoteDTO {
+  file: FileDTO,
+  segments: SegmentDTO[],
+}
+
+export interface SegmentDTO {
+  id: UUID,
+  type: "text" | "diagram",
+}
+
+export interface TextNoteSegmentDTO  {
+  id: UUID,
+  rootNode: TreeNodeDTO,
+  nodes: TreeNodeDTO[]
+}
+
+export interface DiagramNoteSegment {
+  id: UUID,
+  state: LWWStateDTO,
+}
+
+export interface TreeNodeDTO {
+  node: string,
+  parentId: UUID,
+  value: string,
+}
+
+export interface LWWStateDTO {
+  timestamp: number,
+  id: UUID,
+  value : {content: string},
+}
+
+
+
+
---
+++ src/types/file.d.ts
@@ -0,0 +1,13 @@
+import {UUID} from "node:crypto";
+import {FileTypeDTO} from "@/types/dto";
+
+export interface MoaFile {
+  id: UUID;
+  name: string;
+  type: FileTypeDTO;
+  children?: MoaFile[];
+}
+
+export interface Segment {
+
+}
