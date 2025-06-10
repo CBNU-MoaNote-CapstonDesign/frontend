f3e533c50dad4f0ab75d65330844d2b17c9bcfc4
Kim Minseok <semteul32@gmail.com>
2025-06-10 15:01:40 +0900

    feat(diagram): 다이어그램 테스트 동기화 기능 완성 (하드코딩된 UUID)

--- src/components/diagram/DiagramRenderer.tsx
+++ src/components/diagram/DiagramRenderer.tsx
@@ -1,8 +1,9 @@
 "use client"
 import dynamic from "next/dynamic";
-import {useEffect, useState} from "react";
-import type { ExcalidrawImperativeAPI } from "@excalidraw/excalidraw/types";
+import {useEffect, useRef, useState} from "react";
+import type {ExcalidrawImperativeAPI} from "@excalidraw/excalidraw/types";
 import useDocumentSync from "@/hooks/useDocumentSync";
+import {ExcalidrawElement} from "@excalidraw/excalidraw";
 
 
 const ExcalidrawWrapper = dynamic(
@@ -12,44 +13,38 @@ const ExcalidrawWrapper = dynamic(
   },
 );
 
-export default function DiagramRenderer({user, uuid}: { user:User, uuid: string }) {
-  const [excalidrawAPI, setExcalidrawAPI] = useState<ExcalidrawImperativeAPI>();
+export default function DiagramRenderer({user, uuid}: { user: User, uuid: string }) {
+  const updateRef = useRef<null | ((elements: ExcalidrawElement[]) => void)>(null);
 
-  const {publish} = useDocumentSync( user,
+  const {publish} = useDocumentSync(user,
     "01975535-44a6-77eb-84e5-c9b2c27f4e2b",
     (content: string) => {
       try {
 
         const data = JSON.parse(content);
-        if(!excalidrawAPI) {
-          console.log("excalidrawAPI 미준비");
+        console.log("sync에서 데이터가 새로 들어왔습니다");
+        if (!updateRef.current) {
+          console.log("update가 null");
         }
-        if(excalidrawAPI) {
-          console.log("update");
-          excalidrawAPI.updateScene({
-            elements :data,
-            // captureUpdate: CaptureUpdateAction.IMMEDIATELY,
-          });
+        if (updateRef.current) {
+          updateRef.current(data);
         }
-      } catch (e:unknown) {
+      } catch (e: unknown) {
         console.log(e);
       }
     });
 
-  useEffect(() => {
-    if (excalidrawAPI) {
-      console.log("excalidrawAPI 준비완료");
-      excalidrawAPI.onChange((elements) => {
-        console.log("변경 발생");
-        const str = JSON.stringify(elements);
-        publish(str);
-      })
-    }
-  }, [excalidrawAPI]);
-
   return (
     <div className={"w-full h-full"} key={uuid}>
-      <ExcalidrawWrapper setExcalidrawAPI={setExcalidrawAPI}/>
+      <ExcalidrawWrapper
+        setUpdate={(update: ((elements:ExcalidrawElement[])=>void)) => {
+          updateRef.current = update;
+        }}
+        onChange={(elements) => {
+          console.log("변경 발생한거 publish합니다.");
+          const body = JSON.stringify(elements);
+          publish(body);
+        }}/>
     </div>
   );
 }
