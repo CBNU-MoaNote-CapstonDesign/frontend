688324c15e5a64a06c683e4af4f3756afed48193
Sa Jaeheon <jls15900@gmail.com>
2025-06-10 21:13:16 +0900

    design: 홈 화면 타이핑 효과, 페이드 인 효과 적용
    [주의사항]
    새로운 의존성 패키지가 설치되었으니 빌드전 npm install을 할 것

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,27 @@
 import TopNavigationBar from "@/components/layout/Home/TopNavigationBar";
-import Main from "@/components/layout/Home/Main"
-import BasicIntroduce from "@/components/layout/Home/BasicIntroduce"
-import MoaAIIntroduce from "@/components/layout/Home/MoaAIIntroduce"
+import Main from "@/components/layout/Home/Main";
+import BasicIntroduce from "@/components/layout/Home/BasicIntroduce";
+import MoaAIIntroduce from "@/components/layout/Home/MoaAIIntroduce";
 import Footer from "@/components/layout/Home/Footer";
-import ContactLinkBar from "@/components/layout/Home/ContactLinkBar"
+import ContactLinkBar from "@/components/layout/Home/ContactLinkBar";
+import FadeInOnView from "@/components/common/FadeInOnView";
 
 export default function RootPage() {
   return (
-    <div>
-      <div className="flex flex-col justify-start items-center w-full relative gap-[59px] bg-[#f0f8fe]">
-        <TopNavigationBar />
-        <Main />
+    <div className="min-h-screen w-full bg-[#f0f8fe] flex flex-col items-center">
+      <TopNavigationBar />
+      <div className="flex flex-col items-center w-full max-w-6xl px-6 gap-[59px] mt-8">
+        <FadeInOnView delay={0.35} className="w-full">
+          <Main />
+        </FadeInOnView>
+
         <BasicIntroduce />
         <MoaAIIntroduce />
-        <Footer />
+
+        <FadeInOnView delay={0.35}>
+          <Footer />
+        </FadeInOnView>
+
         <ContactLinkBar />
       </div>
     </div>
