a2a42b710b21946cbf73243ad5eea71e507cf364
SONY-STRING <sctoa1@gmail.com>
2025-06-11 21:39:54 +0900

    temp: useFugueDocumentSync 로직을 TreeBasedDocumentRenderer 컴포넌트 내부에 복사하였습니다

--- src/components/document/TreeBasedDocumentRenderer.tsx
+++ src/components/document/TreeBasedDocumentRenderer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {Note} from "@/types/note";
 import {TreeMarkdownEditor} from "@/components/document/TreeMarkdownEditor";
 import {MarkdownRenderer} from "@/components/document/MarkdownRenderer";
@@ -9,6 +9,10 @@ import getDiff from "@/libs/simpledDiff";
 import {CRDTOperation} from "@/types/crdtOperation";
 import useFugueDocumentSync from "@/hooks/useFugueDocumentSync";
 import {TextNoteSegmentDTO} from "@/types/dto";
+import {Client} from "@stomp/stompjs";
+import toast from "react-hot-toast"
+
+const SERVER_WS_URL = process.env.NEXT_PUBLIC_SERVER_WS_URL;
 
 export default function DocumentRenderer({user, uuid}: { user:User, uuid: string }) {
   const [treeNote, setTreeNote] = useState<TreeNote>(TreeNote.fromString(user.id, uuid, "loading...", "loading..."));
@@ -17,6 +21,67 @@ export default function DocumentRenderer({user, uuid}: { user:User, uuid: string
   const [cursorPosition, setCursorPosition] = useState<number>(0); // 커서 위치
   const startEditing = () => setEditing(true);
   const endEditing = () => setEditing(false);
+
+  const clientRef = useRef<Client | null>(null);
+  const textNoteSegmentsRef = useRef<TextNoteSegmentDTO[]>([]);
+
+  /**
+   * broadcast : operation을 백엔드에 전송하는 기능
+   * 백엔드는 해당 operation을 채널에 접속된 모든 유저에게 전송한다.
+   * @param docId
+   * @param segmentId
+   * @param actions CRDTOperation[] - 전송할 operation 배열
+   */
+  const broadcast = (docId:string, segmentId: string, actions: CRDTOperation[]) => {
+    if (clientRef.current && clientRef.current.connected) {
+      clientRef.current.publish({
+        destination: `/app/docs/text/edit/${docId}/${segmentId}`,
+        body: JSON.stringify(actions),
+      });
+    }
+  };
+
+  useEffect(() => {
+    console.log("useFugueDocumentSync useEffect called");
+    const client = new Client({
+      brokerURL: `${SERVER_WS_URL}/docs`,
+      reconnectDelay: 5000,
+      onConnect: () => {
+        if (process.env.DEBUG==='true')
+          toast("연결시도 끝");
+        console.log(`/app/docs/text/participate/${uuid}`);
+        client.subscribe(`/app/docs/text/participate/${uuid}`, (data) => {
+          const body = JSON.parse(data.body);
+          if(body?.textNoteSegments) {
+            textNoteSegmentsRef.current = body.textNoteSegments as TextNoteSegmentDTO[];
+            initialize(textNoteSegmentsRef.current[0]);
+            client.subscribe(`/topic/docs/text/${uuid}/${textNoteSegmentsRef.current[0].id}`, (message) => {
+              try {
+                const body = JSON.parse(message.body);
+                const actions = body as CRDTOperation[];
+                update(actions); // action 전달받았을 때 콜백 호출
+              } catch {
+                toast.error("전달 받은 정보의 body가 Operation 형식이 아닙니다. : " + message.body);
+              }
+            });
+          }
+        }, {'participantUserId': user.id});
+      },
+      onWebSocketError: (event) => {
+        toast.error("서버와의 연결이 실패하였습니다.",{position:"bottom-right"});
+        console.error('WebSocket error:', event);
+      },
+      onStompError: (frame) => {
+        toast.error("서버와 통신 도중 에러 발생",{position:"bottom-right"});
+        console.error('Broker error:', frame.headers['message']);
+      },
+    });
+    client.activate();
+    clientRef.current = client;
+    return () => {
+      client.deactivate();
+    };
+  }, [uuid]);
 
   const commitActions = () => {
     treeNote.traversal();
@@ -52,8 +117,6 @@ export default function DocumentRenderer({user, uuid}: { user:User, uuid: string
     });
     setTreeNote(initialTree);
   }
-
-  const [{broadcast}, ] = useState(useFugueDocumentSync(user.id, uuid, update, initialize));
 
   // 로컬 변경사항을 보내는거
   const send =
